544cbfc56c6d14fb88afa0cdbe20c59cf913b909
Maaj <maajb1122@gmail.com>
2024-08-17 22:20:48 +0530

    committed

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -3,19 +3,19 @@ import React from 'react'
 const SignUp = () => {
   return (
     <div className='flex items-center justify-center mt-6'>
-        <div className='w-5xl h-full p-3 m-3 border rounded-md bg-slate-100 border-white shadow-md'>
-            <div className='p-3 flex flex-col gap-4 '>
+        <div className='w-96 h-full p-6 m-3 border rounded-md bg-slate-100 border-white shadow-md'>
+            <div className='p-3 flex flex-col gap-4'>
                 <div className='text-center'>
-                <h1 className='text-black font-bold font-poppins text-2xl'>Sign Up Option</h1>
+                    <h1 className='text-black font-bold font-poppins text-2xl'>Sign Up Option</h1>
                 </div>
                 <input type="text" placeholder='Enter the username' className='text-black bg-slate-300 p-3 rounded-md'/>
                 <input type="text" placeholder='Enter the email' className='text-black bg-slate-300 p-3 rounded-md'/>
-                <input type="number" placeholder='Enter the phoneno' className='text-black bg-slate-300 p-3 rounded-md'/>
+                <input type="number" placeholder='Enter the phone number' className='text-black bg-slate-300 p-3 rounded-md'/>
                 <input type="password" placeholder='Enter the password' className='text-black bg-slate-300 p-3 rounded-md'/>
-                <input type="password" placeholder='confirm the password' className='text-black bg-slate-300 p-3 rounded-md' />
+                <input type="password" placeholder='Confirm the password' className='text-black bg-slate-300 p-3 rounded-md' />
             </div>
             <div className='text-center'>
-                <button className='bg-blue-700 rounded-3xl p-3 text-semibold text-white '>Sign Up </button>
+                <button className='bg-blue-700 rounded-3xl p-2 m-3 w-28 font-semibold text-white'>Sign Up</button>
             </div>
         </div>
     </div>
