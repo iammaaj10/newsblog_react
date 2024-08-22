7e33fd6ea70bb4b6684cbfd305198722609d82ad
Maaj <maajb1122@gmail.com>
2024-08-22 22:32:44 +0530

    committed

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,62 +1,89 @@
-import React, { useState } from 'react'
-import {createUserWithEmailAndPassword} from 'firebase/auth'
-import{auth,db} from './firebase'
-import { setDoc,doc } from 'firebase/firestore'
-const SignUp = ({onClose1}) => {
-    const [username,setusername]= useState("")
-    const [email,setemail]= useState("")
-    const [phone,setphone]= useState("")
-    const [password,setpassword]= useState("")
+import React, { useState } from 'react';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { auth, db } from './firebase';
+import { setDoc, doc } from 'firebase/firestore';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-    const handleregister = async(e)=>{
-        e.preventDefault()
-        try {
-            await  createUserWithEmailAndPassword(auth,email,password)
-            const user = auth.currentUser
-            console.log(user);
-            if(user)
-            {
-                await setDoc(doc(db,"Users",user.uid),{
-                    email:user.email,
-                    username:username,
-                    phno:phone
-                })
-            }
-            console.log("Registration done ");
-            
-            
-        } 
-        catch (error) {
-           console.log(error.message);
-            
-        }
+const SignUp = ({ onClose1 }) => {
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [password, setPassword] = useState('');
 
+  const handleRegister = async (e) => {
+    e.preventDefault();
+    try {
+      await createUserWithEmailAndPassword(auth, email, password);
+      const user = auth.currentUser;
+      console.log(user);
+      if (user) {
+        await setDoc(doc(db, 'Users', user.uid), {
+          email: user.email,
+          username: username,
+          phno: phone,
+        });
+      }
+      toast.success('User Registered Successfully', { position: 'top-center'  });
+    } catch (error) {
+      console.log(error.message);
+      toast.error(error.message, { position: 'bottom-center' });
     }
+  };
+
   return (
     <div className='flex items-center justify-center mt-6 fixed inset-0 backdrop-blur-sm bg-opacity-30 z-10'>
-        <div className='w-96 p-6 m-3 border rounded-md bg-slate-100 border-white shadow-md relative'>
-        <i class="fa-regular fa-circle-xmark right-2 absolute text-black top-2 font-bold text-lg hover:text-orange-600 cursor-pointer" onClick={onClose1}></i>
-        <form action="" onSubmit={handleregister}>
-            <div className='p-3 flex flex-col gap-4'>
-            
-                <div className='text-center'>
-                    <h1 className='text-black font-bold font-poppins text-2xl'>Sign Up Option</h1>
-                </div>
-            
-                <input type="text" placeholder='Enter the username' className='text-black bg-slate-100 p-3 rounded-md' onChange={(e)=>setusername( e.target.value)} required/>
-                <input type="text" placeholder='Enter the email' className='text-black bg-slate-100 p-3 rounded-md' onChange={(e)=>setemail( e.target.value)} required/>
-                <input type="number" placeholder='Enter the phone number' className='text-black bg-slate-100 p-3 rounded-md' onChange={(e)=>setphone( e.target.value)} required/>
-                <input type="password" placeholder='Enter the password' className='text-black bg-slate-100 p-3 rounded-md' onChange={(e)=>setpassword( e.target.value)} required/>
-                
-                
-            </div>
+      <div className='w-96 p-6 m-3 border rounded-md bg-slate-100 border-white shadow-md relative'>
+        <i
+          className='fa-regular fa-circle-xmark right-2 absolute text-black top-2 font-bold text-lg hover:text-orange-600 cursor-pointer'
+          onClick={onClose1}
+        ></i>
+        <form action='' onSubmit={handleRegister}>
+          <div className='p-3 flex flex-col gap-4'>
             <div className='text-center'>
-                <button className='bg-blue-700 rounded-3xl p-2 m-3 w-28 font-semibold text-white hover:bg-orange-500 hover:text-black cursor-pointer'>Sign Up</button>
+              <h1 className='text-black font-bold font-poppins text-2xl'>
+                Sign Up Option
+              </h1>
             </div>
-            </form>
-        </div>
+            <input
+              type='text'
+              placeholder='Enter the username'
+              className='text-black bg-slate-100 p-3 rounded-md'
+              onChange={(e) => setUsername(e.target.value)}
+              required
+            />
+            <input
+              type='text'
+              placeholder='Enter the email'
+              className='text-black bg-slate-100 p-3 rounded-md'
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+            <input
+              type='number'
+              placeholder='Enter the phone number'
+              className='text-black bg-slate-100 p-3 rounded-md'
+              onChange={(e) => setPhone(e.target.value)}
+              required
+            />
+            <input
+              type='password'
+              placeholder='Enter the password'
+              className='text-black bg-slate-100 p-3 rounded-md'
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+          <div className='text-center'>
+            <button className='bg-blue-700 rounded-3xl p-2 m-3 w-28 font-semibold text-white hover:bg-orange-500 hover:text-black cursor-pointer'>
+              Sign Up
+            </button>
+          </div>
+        </form>
+        <ToastContainer autoClose={2000}  autoOpen={1000}/>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default SignUp
+export default SignUp;
