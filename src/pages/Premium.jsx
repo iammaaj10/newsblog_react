f580a3b9193a8711deb1c7462229bcd98458f300
Maaj <maajb1122@gmail.com>
2024-08-30 20:08:52 +0530

    committed

--- src/pages/Premium.jsx
+++ src/pages/Premium.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import PDetails from '../components/PDetails'
+import Content from '../components/Content'
 
 const Premium = () => {
   return (
     <div className='bg-black h-screen'>
       <PDetails/>
+      <Content/>
     </div>
   )
 }
